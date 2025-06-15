001a2932c8b49852c6da0045379c33cd83d3f6bf
visanalexandru <alexandruvisan44@yahoo.com>
2025-06-15 13:12:09 +0300

    Filter keypoints not showing in other camera

--- utils.py
+++ utils.py
@@ -21,38 +21,64 @@ def distance_point_to_line(point, line):
     x0, y0 = point
     return abs(a * x0 + b * y0 + c) / np.sqrt(a**2 + b**2)
 
-def get_bbox_center(bbox):
-    x1, y1, x2, y2 = bbox
-    return np.array([(x1 + x2) / 2, (y1 + y2) / 2])
 
-def get_assignment(objects1, objects2, frame1, frame2, fundamentalMatrix):
-    costs = np.ones((len(objects1), len(objects2))) * 3000
+# Iterates through all the object bounding boxes and extracts a point
+# in the bounding box to be used as a keypoint. It's usually a point on
+# the lowest part of the bounding box. This way, it's approximately on the
+# street, so we can apply the homography matrix to map it to another camera
+# view.
+def get_keypoints(frame, homography):
+    objects = frame.get_objects()
+    keypoints = []
 
-    for i, object1 in enumerate(objects1): 
-        bounding_box1 = object1["bbox"]
-        center1 = get_bbox_center(bounding_box1)
+    for object in objects:
+        x1, y1, x2, y2 = object["bbox"]
 
-        for j, object2 in enumerate(objects2):
-            bounding_box2 = object2["bbox"]
-            center2 = get_bbox_center(bounding_box2)
+        keypoint = np.array(((x1+x2)//2, y2)).reshape(1, 1, 2).astype(np.float32)
+        transformed_keypoint = cv.perspectiveTransform(keypoint, homography).squeeze()
+        transformed_keypoint = transformed_keypoint.astype(np.int32)
 
-            if object1["class"] != object2["class"]:
-                costs[i][j] = 3000
-                continue
+        # Don't include those keypoints that are not visible in the second camera.
+        if transformed_keypoint[0] < 0 or transformed_keypoint[0] >= FRAME_WIDTH:
+            continue
+        if transformed_keypoint[1] < 0 or transformed_keypoint[1] >= FRAME_HEIGHT:
+            continue
             
-            epiline = cv.computeCorrespondEpilines(np.array([center1]), 1, fundamentalMatrix)[0][0]
-            distance = distance_point_to_line(center2, epiline)
-            if distance > 50:
-                costs[i, j] = 3000
-            else:
-                costs[i, j] = distance
+        keypoints.append({
+            "position": ((x1+x2)//2, y2),
+            "class": object["class"]
+        })
+
+    return keypoints
+
+def get_assignment(keypoints1, keypoints2, homography):
+    max_distance = np.linalg.norm([FRAME_WIDTH, FRAME_HEIGHT])
+    costs = np.ones((len(keypoints1), len(keypoints2))) * max_distance 
+
+    for i, a in enumerate(keypoints1): 
+        for j, b in enumerate(keypoints2):
+            pos1 = a["position"]
+            pos2 = b["position"]
+
+            if a["class"] != b["class"]:
+                costs[i, j] = max_distance 
+                continue
+           
+            pos1_tmp = np.array([[pos1]]).astype(np.float32)
+            transformed = cv.perspectiveTransform(pos1_tmp, homography).squeeze()
+
+            distance = np.linalg.norm(transformed - np.array(pos2))
+            costs[i, j] = distance
 
     assignment = linear_sum_assignment(costs) 
 
     indices_1, indices_2 = assignment
     cost = costs[indices_1, indices_2].sum()
 
-    unmatched = [i for i in range(len(objects1)) if i not in indices_1]
-    cost += len(unmatched) * 3000  # Penalize unmatched objects
+    unmatched = [i for i in range(len(keypoints1)) if i not in indices_1]
+    cost += len(unmatched) * max_distance # Penalize unmatched objects
+
+    unmatched = [i for i in range(len(keypoints2)) if i not in indices_2]
+    cost += len(unmatched) * max_distance # Penalize unmatched objects
 
     return assignment, cost, unmatched
